d3e1370295d288d81c26df7019aa182c66986613
pammacdotnet <pammac@ACorbiMBA.local>
2013-08-30 22:36:37 +0200

    Add empty Menu class

---
+++ MacGap/Menus.h
@@ -0,0 +1,13 @@
+//
+//  Menus.h
+//  MacGap
+//
+//  Created by Alberto Corbi Bellot on 30/08/13.
+//  Copyright (c) 2013 Twitter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Menus : NSObject
+
+@end
---
+++ MacGap/Menus.m
@@ -0,0 +1,13 @@
+//
+//  Menus.m
+//  MacGap
+//
+//  Created by Alberto Corbi Bellot on 30/08/13.
+//  Copyright (c) 2013 Twitter. All rights reserved.
+//
+
+#import "Menus.h"
+
+@implementation Menus
+
+@end
